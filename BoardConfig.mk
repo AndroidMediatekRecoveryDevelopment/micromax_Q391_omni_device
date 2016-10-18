cc99364659735467ec4d2de16e068839f8c14128
kirito96 <aquammie96@gmail.com>
2016-10-18 14:25:16 -0400

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,55 @@
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := Q391
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6572
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/micromax/Q391/kernel
+BOARD_KERNEL_CMDLINE := 
+BOARD_MKBOOTIMG_ARGS := --base 0x96000000 --pagesize 2048 --kernel_offset 0x00009600 --ramdisk_offset 0x04000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --board 1429947554 --mtk 1
+BOARD_CUSTOM_MKBOOTIMG := mtkbootimg
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := device/micromax/Q391/bootimg.mk
+
+# USB Mounting
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun0/file
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_FLASH_BLOCK_SIZE := 512
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+
+# Device-Specific
+DEVICE_RESOLUTION := 540 x 960
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+# TWRP-Specific
+TARGET_RECOVERY_FSTAB := device/micromax/Q391/recovery/etc/twrp.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_THEME := portrait_mdpi
+TW_EXCLUDE_MTP := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_NO_CPU_TEMP := true
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 255
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
---
+++ omni_Q391.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/micromax/Q391/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Q391
+PRODUCT_NAME := omni_Q391
+PRODUCT_BRAND := micromax
+PRODUCT_MANUFACTURER := micromax
+PRODUCT_MODEL := Q391
